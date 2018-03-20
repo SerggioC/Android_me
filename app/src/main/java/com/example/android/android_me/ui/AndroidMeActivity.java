89cf898fd679d65a1d2c7ec60f2b8fb46532e3e5
SerggioC <sergiocrz@gmail.com>
2018-03-20 13:38:40 +0000

    Adding fragments + swype/Fling gestures to change images

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,11 +16,16 @@
 
 package com.example.android.android_me.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -31,14 +36,144 @@ public class AndroidMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
+        if (savedInstanceState != null) return;
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         // Create a new head BodyPartFragment
         BodyPartFragment headFragment = new BodyPartFragment();
 
+        headFragment.setListBodyParts(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(0);
         // Add the fragment to its container using a FragmentManager and a Transaction
-        FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
                 .add(R.id.head_container, headFragment)
                 .commit();
+
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        bodyFragment.setListBodyParts(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(0);
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container, bodyFragment)
+                .commit();
+
+        BodyPartFragment legsFragment = new BodyPartFragment();
+        legsFragment.setListBodyParts(AndroidImageAssets.getLegs());
+        legsFragment.setListIndex(0);
+        fragmentManager.beginTransaction()
+                .add(R.id.feet_container, legsFragment)
+                .commit();
+
     }
+
+
+    void testLists() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        List<Integer> list = new ArrayList<Integer>();
+
+
+        // ArrayList add
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(i);
+        }
+        long duration = System.nanoTime() - startTime;
+        System.out.println("ArrayList add:  \n" + duration);
+
+        // ArrayList get
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            arrayList.get(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("ArrayList get:  \n" + duration);
+
+        // ArrayList set
+        startTime = System.nanoTime();
+        for (int i = 99999; i >= 0; i--) {
+            arrayList.set(i, i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("ArrayList set:  \n" + duration);
+
+        // ArrayList remove
+        startTime = System.nanoTime();
+        for (int i = 99999; i >= 0; i--) {
+            arrayList.remove(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("ArrayList remove:  \n" + duration);
+
+
+        // List add
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            list.add(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("List add:  \n" + duration);
+
+        // List get
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            list.get(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("List get:  \n" + duration);
+
+        // List set
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            list.set(i, i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("List set:  \n" + duration);
+
+        // List remove
+        startTime = System.nanoTime();
+        for (int i = 99999; i >= 0; i--) {
+            list.remove(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("List remove: \n" + duration);
+
+
+        // LinkedList add
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.add(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("LinkedList add: \n" + duration);
+
+        // LinkedList get
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.get(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("LinkedList get: \n" + duration);
+
+        // LinkedList set
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            linkedList.set(i, i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("LinkedList set:  \n" + duration);
+
+        // LinkedList remove
+        startTime = System.nanoTime();
+        for (int i = 99999; i >= 0; i--) {
+            linkedList.remove(i);
+        }
+        duration = System.nanoTime() - startTime;
+        System.out.println("LinkedList remove:  \n" + duration);
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -17,21 +17,47 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.android.android_me.R;
-import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BodyPartFragment extends Fragment {
+
+    private static final String ARRAY_LIST_BUNDLE = "ArrayList_Bundle";
+    private static final String LIST_INDEX_BUNDLE = "List_Index_Bundle";
+    VelocityTracker mVelocityTracker = null;
+    private List<Integer> mListBodyParts;
+    private Integer mListIndex = 0;
+    private GestureDetector gestureDetector;
+    private View.OnClickListener clickListener;
+    private ImageView imageView;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the fragment
      */
     public BodyPartFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mListBodyParts = savedInstanceState.getIntegerArrayList(ARRAY_LIST_BUNDLE);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX_BUNDLE);
+        }
     }
 
     /**
@@ -44,13 +70,186 @@ public class BodyPartFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
 
         // Get a reference to the ImageView in the fragment layout
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateViewOnClick(1);
+                Log.i("Sergio>", this + " onClick normal");
+            }
+        };
+        imageView.setOnClickListener(clickListener);
+
+        gestureDetector = new GestureDetector(getContext(), new MyGestureListener());
+
+        // This touch listener passes everything on to the gesture detector.
+        // That saves us the trouble of interpreting the raw touch events
+        // ourselves.
+        View.OnTouchListener touchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // pass the events to the gesture detector
+                // a return value of true means the detector is handling it
+                // a return value of false means the detector didn't
+                // recognize the event
+                return gestureDetector.onTouchEvent(event);
+
+            }
+        };
+
+        imageView.setOnTouchListener(touchListener);
+
 
         // Set the image to the first in our list of head images
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        if (mListBodyParts != null && mListIndex >= 0) {
+            imageView.setImageResource(mListBodyParts.get(mListIndex));
+        } else {
+            imageView.setImageResource(mListBodyParts.get(0));
+            mListIndex = 0;
+        }
+
 
         // Return the rootView
         return rootView;
     }
 
+    private void updateViewOnClick(Integer direction) { // direction: +/-1
+        mListIndex += direction;
+        int listSize_1 = mListBodyParts.size() - 1;
+        if (mListIndex > listSize_1) {
+            mListIndex = 0;
+        } else if (mListIndex < 0) {
+            mListIndex = listSize_1;
+        }
+        imageView.setImageResource(mListBodyParts.get(mListIndex));
+    }
+
+    public void setListBodyParts(List<Integer> mListBodyParts) {
+        this.mListBodyParts = mListBodyParts;
+    }
+
+    public void setListIndex(Integer mListIndex) {
+        this.mListIndex = mListIndex;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntegerArrayList(ARRAY_LIST_BUNDLE, (ArrayList<Integer>) mListBodyParts);
+        outState.putInt(LIST_INDEX_BUNDLE, mListIndex);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            updateViewOnClick(1);
+            Log.i("Sergio>", this + " onSingleTapUp");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.i("Sergio>", this + " onLongPress");
+            super.onLongPress(e);
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+//            float distance = e1.getX() - e2.getX();
+//            Log.v("Sergio>", this + " onScroll distance= " + distance);
+//
+//            float speed = Math.abs(distance / (e1.getEventTime() - e2.getEventTime()));
+//            Log.i("Sergio>", this + " onScroll speed= " + speed);
+//
+//            if (e1.getX() < e2.getX() && Math.abs(distanceX) > 50 && speed > 0.3) {
+//                updateViewOnClick(1);
+//                Log.w("Sergio>", "Left to Right scroll performed 0]--->>> " + distanceX);
+//            }
+//
+//            if (e1.getX() > e2.getX() && Math.abs(distanceX) > 50 && speed > 0.3) {
+//                updateViewOnClick(-1);
+//                Log.w("Sergio>", "Right to Left scroll performed  <<<---[0 " + distanceX);
+//            }
+
+
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            int PIXELS_PER_SECOND = 100;
+            float maxFlingVelocity = ViewConfiguration.get(getContext()).getScaledMaximumFlingVelocity();
+            float velocityPercentX = velocityX / maxFlingVelocity;          // the percent is a value in the range of (0, 1]
+            float normalizedVelocityX = velocityPercentX * PIXELS_PER_SECOND;  // where PIXELS_PER_SECOND is a device-independent measurement
+
+
+            if (e1.getY() <= e2.getY() && e1.getX() > e2.getX()) {
+                updateViewOnClick(-1);
+                Log.w("Sergio>", "Up to Down + Right to Left swipe performed");
+            }
+
+            if (e1.getY() <= e2.getY() && e1.getX() < e2.getX()) {
+                updateViewOnClick(1);
+                Log.w("Sergio>", "Up to Down + Left to Right swipe performed");
+            }
+
+            if (e1.getY() >= e2.getY() && e1.getX() < e2.getX()) {
+                updateViewOnClick(1);
+                Log.w("Sergio>", "Down to Up + Left to Right swipe performed");
+            }
+
+            if (e1.getY() >= e2.getY() && e1.getX() > e2.getX()) {
+                updateViewOnClick(-1);
+                Log.w("Sergio>", "Down to Up + Right to Left swipe performed");
+            }
+
+            Log.i("Sergio>", this + " onFling velocity X" + velocityX + "\n" +
+                    "e1.getX()=" + e1.getX() + "\n" +
+                    "e2.getX()=" + e2.getX() + "\n" +
+                    "e1.getY()=" + e1.getY() + "\n" +
+                    "e2.getY()=" + e2.getY());
+
+
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Log.i("Sergio>", this + " onShowPress");
+            super.onShowPress(e);
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.i("Sergio>", this + " onDown");
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.i("Sergio>", this + " onDoubleTap");
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Log.i("Sergio>", this + " onDoubleTapEvent");
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.i("Sergio>", this + " onSingleTapConfirmed");
+            return true;
+        }
+
+        @Override
+        public boolean onContextClick(MotionEvent e) {
+            Log.i("Sergio>", this + " onContextClick");
+            return true;
+        }
+    }
 }
